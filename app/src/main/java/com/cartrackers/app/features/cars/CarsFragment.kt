2531dfd8c5bb8db81516986f58fa676a4fc9266f
JohnJake Talledo <johnjake.talledo2000@gmail.com>
2021-04-23 12:27:31 +0800

    cars to profile

--- app/src/main/java/com/cartrackers/app/features/cars/CarsFragment.kt
+++ app/src/main/java/com/cartrackers/app/features/cars/CarsFragment.kt
@@ -6,21 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.cartrackers.app.data.vo.CarModel
 import com.cartrackers.app.data.vo.State
 import com.cartrackers.app.databinding.FragmentCarsBinding
+import com.cartrackers.app.extension.showNavigation
 import com.cartrackers.app.features.cars.adapter.CarsAdapter
+import com.cartrackers.app.features.common.ProfileOnClickListener
+import com.cartrackers.app.features.main.CarTrackActivity
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 import timber.log.Timber
 
-class CarsFragment: Fragment() {
+class CarsFragment: Fragment(), ProfileOnClickListener {
     private var binding: FragmentCarsBinding? = null
     private val bind get() = binding
-    private val carAdapter: CarsAdapter by lazy { CarsAdapter() }
+    private val carAdapter: CarsAdapter by lazy { CarsAdapter(this) }
     private val viewModel: ViewModel by inject()
     private var stateJob: Job? = null
     private lateinit var resultLayout: LinearLayoutManager
@@ -46,6 +50,11 @@ class CarsFragment: Fragment() {
                 handleStateCars(state)
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        bottomVisibility()
     }
 
     private fun handleStateCars(state: State<List<CarModel>>) {
@@ -85,5 +94,18 @@ class CarsFragment: Fragment() {
             layoutManager = resultLayout
             adapter = carAdapter
         }
+    }
+
+    private fun bottomVisibility() {
+        if(CarTrackActivity.onBackPress) {
+            CarTrackActivity.onBackPress = false
+            activity.showNavigation()
+        }
+    }
+
+    override fun onClickListener(userId: Int) {
+        val args = CarsFragmentDirections.actionCarsToProfile(userId)
+        CarTrackActivity.onBackPress = true
+        view?.findNavController()?.navigate(args)
     }
 }
