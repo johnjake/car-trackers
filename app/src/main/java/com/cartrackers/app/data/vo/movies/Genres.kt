f4b291294d89fcc5f7effb9c7f23da97b0af451e
JohnJake Talledo <johnjake.talledo2000@gmail.com>
2021-05-03 09:20:52 +0800

    data class parcelize

--- app/src/main/java/com/cartrackers/app/data/vo/movies/Genres.kt
+++ app/src/main/java/com/cartrackers/app/data/vo/movies/Genres.kt
@@ -1,7 +1,7 @@
 package com.cartrackers.app.data.vo.movies
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Genres(
